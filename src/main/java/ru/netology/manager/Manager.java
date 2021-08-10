2d4908b705151d4f64bc08b439ba50e03d985b11
e.fedoseeva <tetstest@list.ru>
2021-08-10 04:38:42 +0300

    fixed code

--- src/main/java/ru/netology/manager/Manager.java
+++ src/main/java/ru/netology/manager/Manager.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 public class Manager {
     Repository repository = new Repository();
     private String from;
+    private String to;
 
     public void save(Offer offer) {
         repository.add(offer);
@@ -16,8 +17,8 @@ public class Manager {
     public Offer[] search(String from, String to) {
         Offer[] result = new Offer[0];
         for (Offer offer : repository.findAll()) {
-            Offer[] tmp = new Offer[result.length + 1];
-            if (offer.getDepartureAirport().equals(from.toUpperCase()) || offer.getArrivalAirport().equals(to.toUpperCase())) {
+            if (offer.getDepartureAirport().equals(from.toUpperCase()) && offer.getArrivalAirport().equals(to.toUpperCase())) {
+                Offer[] tmp = new Offer[result.length + 1];
                 System.arraycopy(result, 0, tmp, 0, result.length);
                 tmp[tmp.length - 1] = offer;
                 result = tmp;
--- src/test/java/ru/netology/manager/ManagerTest.java
+++ src/test/java/ru/netology/manager/ManagerTest.java
@@ -4,15 +4,23 @@ package ru.netology.manager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Offer;
+import ru.netology.repository.Repository;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class ManagerTest {
+    private Repository repository = new Repository();
     private final Manager manager = new Manager();
     Offer offer1 = new Offer(1, 10014, "LED", "IKT", 375);
-    Offer offer2 = new Offer(2, 1799, "LED", "SVO", 95);
+    Offer offer2 = new Offer(2, 1799, "LED", "AMS", 175);
     Offer offer3 = new Offer(3, 9625, "VKO", "LCA", 225);
-    Offer offer4 = new Offer(4, 7005, "LED", "SVO", 95);
+    Offer offer4 = new Offer(4, 7005, "LED", "AMS", 175);
+    Offer offer5 = new Offer(5, 10014, "LED", "AMS", 175);
+    Offer offer6 = new Offer(6, 14590, "LED", "AMS", 175);
+    Offer offer7 = new Offer(7, 8655, "LED", "AMS", 175);
+    Offer offer8 = new Offer(8, 7905, "LED", "AMS", 175);
 
     @BeforeEach
     public void save() {
@@ -20,33 +28,40 @@ class ManagerTest {
         manager.save(offer2);
         manager.save(offer3);
         manager.save(offer4);
+        manager.save(offer5);
+        manager.save(offer6);
+        manager.save(offer7);
+        manager.save(offer8);
     }
 
     @Test
-    void searchFromTo(){
+    void searchFromTo() {
         Offer[] expected = new Offer[]{offer3};
         Offer[] actual = manager.search("VKO", "LCA");
         assertArrayEquals(expected, actual);
     }
 
     @Test
-    void searchFromOnly(){
-        Offer[] expected = new Offer[]{offer3};
-        Offer[] actual = manager.search("VKO", " ");
+    void searchAndSort() {
+        Offer[] expected = new Offer[]{offer2, offer4, offer8, offer7, offer5, offer6};
+        Offer[] actual = manager.search("LED", "AMS");
+        Arrays.sort(actual);
         assertArrayEquals(expected, actual);
     }
 
     @Test
-    void searchToOnly(){
-        Offer[] expected = new Offer[]{offer1};
-        Offer[] actual = manager.search(" ", "IKT");
+    void searchNoResult() {
+        Offer[] expected = new Offer[0];
+        Offer[] actual = manager.search("no", "no");
         assertArrayEquals(expected, actual);
     }
 
     @Test
-    void searchAll(){
-        Offer[] expected = new Offer[]{};
-        Offer[] actual = manager.search(" ", " ");
+    void shouldSort(){
+        Offer[] expected = new Offer[]{offer2, offer4, offer8, offer7, offer5, offer6};
+        Offer[] actual = new Offer[]{offer2, offer4, offer5, offer6, offer7, offer8};
+        Arrays.sort(actual);
         assertArrayEquals(expected, actual);
+
     }
 }
